7d926b1b2ac69eb607889f2979753b77cbe3fa43
shreyasbarik <shreyascreative59@gmail.com>
2025-06-09 16:29:12 +0530

    finall call

--- auth.js
+++ auth.js
@@ -1,68 +1,21 @@
 // auth.js
-
 import { auth } from './firebase-config.js';
-import { 
-  createUserWithEmailAndPassword, 
-  signInWithEmailAndPassword, 
-  onAuthStateChanged, 
-  signOut 
-} from 'firebase/auth';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
 
-// Signup function
-const signupForm = document.querySelector('#signup-form');
-if (signupForm) {
-  signupForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const email = signupForm['signup-email'].value;
-    const password = signupForm['signup-password'].value;
+// Export functions to handle login/signup
 
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(cred => {
-        console.log('User signed up:', cred.user);
-        signupForm.reset();
-        window.location.href = 'index.html';
-      })
-      .catch(err => {
-        alert(err.message);
-      });
-  });
+export function signup(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
 }
 
-// Login function
-const loginForm = document.querySelector('#login-form');
-if (loginForm) {
-  loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const email = loginForm['login-email'].value;
-    const password = loginForm['login-password'].value;
-
-    signInWithEmailAndPassword(auth, email, password)
-      .then(cred => {
-        console.log('User logged in:', cred.user);
-        loginForm.reset();
-        window.location.href = 'index.html';
-      })
-      .catch(err => {
-        alert(err.message);
-      });
-  });
+export function login(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
 }
 
-// Logout function
-const logoutBtn = document.querySelector('#logout');
-if (logoutBtn) {
-  logoutBtn.addEventListener('click', () => {
-    signOut(auth).then(() => {
-      window.location.href = 'index.html';
-    });
-  });
+export function onAuthChange(callback) {
+  onAuthStateChanged(auth, callback);
 }
-
-// Auth state listener (optional)
-onAuthStateChanged(auth, user => {
-  if (user) {
-    console.log('User is logged in:', user);
-  } else {
-    console.log('User is logged out');
-  }
-});
